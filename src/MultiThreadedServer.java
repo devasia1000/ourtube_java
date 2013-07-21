f0f1122dae9098944c202808de8ba1c4faa449fc
Devasia Manuel <devasia1000@gmail.com>
2013-07-20 21:12:35 -0400

    using ByteArrayOutputStream

--- src/MultiThreadedServer.java
+++ src/MultiThreadedServer.java
@@ -9,25 +9,34 @@ import java.util.ArrayList;
 
 public class MultiThreadedServer implements Runnable {
    Socket sock;
-   MultiThreadedServer(Socket csocket) {
-      this.sock = csocket;
-   }
+    MultiThreadedServer(Socket csocket) {
+        this.sock = csocket;
+    }
 
-   @Override
-   public void run() {
-      try {
-          /* open input stream and read all the bytes */
-          InputStreamReader clientReader=new InputStreamReader(sock.getInputStream());
-          char[] bytesReadFromClient=null;
-          clientReader.read(bytesReadFromClient);
-          clientReader.close();
-          
-          /* parse the request */
-          System.out.println(new String(bytesReadFromClient));
-          HTTPParser parse=new HTTPParser(new String(bytesReadFromClient));
-      }
-      catch (Exception e) {
-         System.out.println(e);
-      }
-   }
+    @Override
+    public void run() {
+        try {
+            /* open input stream and read all the bytes */
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            InputStream is=sock.getInputStream();
+            
+            byte[] buf = new byte[4096];
+            while (true) {
+                int n = is.read(buf);
+                if (n < 0) {
+                    break;
+                }
+                baos.write(buf, 0, n);
+            }
+
+            byte data[] = baos.toByteArray();
+
+            /* parse the request */
+            System.out.println(new String(data));
+            HTTPParser parse = new HTTPParser(new String(data));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
