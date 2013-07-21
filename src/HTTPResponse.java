f977d68e90d0a350e77a953ce6a5e3800298ce4d
Devasia Manuel <devasia1000@gmail.com>
2013-07-20 23:03:51 -0400

    convert HTTPResponse to byte array

--- src/HTTPResponse.java
+++ src/HTTPResponse.java
@@ -2,16 +2,30 @@
  *
  * @author Devasia
  */
-
 import java.util.*;
 
 public class HTTPResponse {
 
-    ArrayList<HTTPHeader> headerList=new ArrayList<HTTPHeader>();
-    
-    public HTTPResponse(String mess, byte[] b){
-        
+    private HTTPParser parser;
+    private byte[] binaryData;
+
+    public HTTPResponse(String mess, byte[] b) {
+        parser = new HTTPParser(mess);
+        binaryData = b;
     }
-    
-    
+
+    public HTTPParser returnParser() {
+        return parser;
+    }
+
+    public byte[] toHTTPBytes() {
+        byte[] one = parser.toHTTPString().getBytes();
+        byte[] two = binaryData;
+        byte[] combined = new byte[one.length + two.length];
+
+        System.arraycopy(one, 0, combined, 0, one.length);
+        System.arraycopy(two, 0, combined, one.length, two.length);
+        
+        return combined;
+    }
 }
