d56241a8b59004a52ba7174013521192625eab5f
Devasia Manuel <devasia1000@gmail.com>
2013-07-21 18:49:14 -0400

    added error handling

--- src/MultiThreadedServer.java
+++ src/MultiThreadedServer.java
@@ -5,6 +5,7 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 public class MultiThreadedServer implements Runnable {
    Socket sock;
@@ -34,8 +35,8 @@ public class MultiThreadedServer implements Runnable {
              * 
              * IMPORTANT: changed host to usatoday.com to prevent redirect loop, 
              * change back to 'parser.returnHost()' after tests*/
-            Socket s=new Socket(parser.returnHost(), 80);
-            //Socket s = new Socket("usatoday.com", 80);
+            //Socket s=new Socket(parser.returnHost(), 80);
+            Socket s = new Socket("4.53.56.89", 80);
             BufferedWriter wt = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
             wt.write(parser.toHTTPString());
             wt.flush();
@@ -45,9 +46,14 @@ public class MultiThreadedServer implements Runnable {
             /* we're using a 1MB byte array to store data...will that be enough? what is we need to store more data? */
             byte[] data=new byte[1048576];
             int dataRead=binaryReader.read(data);
+            //System.out.print(new String(data));
+            /* throw an exception if no response is read from server */
+            if(dataRead==0){
+                System.err.println("no response read from server");
+                throw new Exception();
+            }
             /* move binary data into smaller byte array */
-            byte[] data_small=new byte[dataRead];
-            System.arraycopy(data, 0, data_small, 0, dataRead);
+            byte[] data_small= Arrays.copyOfRange(data, 0, dataRead);
             /* mark larger byte array for garbage collection */
             data=null;
             
