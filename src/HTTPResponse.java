f80290f4804c837b7399f0e29f6fcdf022a47687
Devasia Manuel <devasia1000@gmail.com>
2013-07-21 18:01:00 -0400

    take care of corner case where response has no body

--- src/HTTPResponse.java
+++ src/HTTPResponse.java
@@ -15,18 +15,26 @@ public class HTTPResponse {
 
     public HTTPResponse(byte[] b) {
         totalBinaryData = b;
+        boolean responseDataAvailable = true;
 
         for (int i = 0; i < b.length; i++) {
-            if (b[i] == 13 && b[i+1]==10 && b[i+2]==13 && b[i+3]==10) {
+            if (b[i] == 13 && b[i + 1] == 10 && b[i + 2] == 13 && b[i + 3] == 10) {
                 System.out.println("found end of headers");
                 binaryDataStartPosition = i + 4;
+
+                if (binaryDataStartPosition >= b.length - 1) {
+                    responseDataAvailable = false;
+                }
             }
         }
 
-        headers = Arrays.copyOfRange(b, 0, binaryDataStartPosition - 1);
-        data = Arrays.copyOfRange(b, binaryDataStartPosition, b.length);
-        
-        parser=new HTTPParser(new String(headers));
+        if (responseDataAvailable) {
+            headers = Arrays.copyOfRange(b, 0, binaryDataStartPosition - 1);
+            data = Arrays.copyOfRange(b, binaryDataStartPosition, b.length);
+
+            parser = new HTTPParser(new String(headers));
+        }
+
     }
 
     public HTTPParser returnParser() {
