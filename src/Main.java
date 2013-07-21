5e1c1723a8a4cecb06deabb17481ec5040b81dc1
Devasia Manuel <devasia1000@gmail.com>
2013-07-20 20:28:36 -0400

    Created main class

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author Devasia
+ */
+
+import java.net.*;
+
+public class Main {
+
+    public static void main(String args[])
+            throws Exception {
+        ServerSocket ssock = new ServerSocket(1234);
+        System.out.println("Listening");
+        while (true) {
+            Socket sock = ssock.accept();
+            System.out.println("Connected");
+            new Thread(new MultiThreadServer(sock)).start();
+        }
+    }
+}
