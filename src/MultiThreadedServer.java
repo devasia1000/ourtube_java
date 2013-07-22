2f949da9a5112a6918c3db8551957c9e833e72bb
Tiffany Chen <yuhan210@gmail.com>
2013-07-22 12:34:43 -0400

    use InputStream to read data from client

--- src/MultiThreadedServer.java
+++ src/MultiThreadedServer.java
@@ -1,47 +1,51 @@
+
 /**
  *
  * @author Devasia
  */
-
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
 
 public class MultiThreadedServer implements Runnable {
-   Socket sock;
+
+    Socket sock;
+
     MultiThreadedServer(Socket csocket) {
         this.sock = csocket;
     }
 
     @Override
     public void run() {
-        try {            
-            /* open client input stream and read all the bytes */
-            BufferedReader rd=new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            String line=null, mess="";
-            while((line=rd.readLine())!=null){
-                if(line.equals("")){
-                    break;
-                }
-                else{
-                    mess=mess+line+"\n";
-                }
-            }
-            
-            if(mess==null){
-                System.err.println("could not read any data from client");
+        try {
+
+            /* keep these two streams open */
+            InputStream rd = sock.getInputStream();
+            DataOutputStream os = new DataOutputStream(sock.getOutputStream());
+
+            //while (true) {
+            byte[] binaryRequest = new byte[1048576];
+            //System.out.println("going to read");
+            int numBytesRead = rd.read(binaryRequest);
+            if (numBytesRead == 0 || numBytesRead == -1) {
+                System.err.println("could not read request");
                 throw new Exception();
             }
+            //System.out.println(numBytesRead);
+            byte[] requestBinary_small = Arrays.copyOfRange(binaryRequest, 0, numBytesRead);
+            String mess = new String(requestBinary_small);
+            mess = mess.replaceAll("\r\n", "\n");
+            //System.out.print(mess);
 
             /* parse HTTPRequest */
-            HTTPRequest request=new HTTPRequest(mess);
-            
+            HTTPRequest request = new HTTPRequest(mess);
+
             /* open socket to server and make request 
              * 
              * IMPORTANT: changed host to usatoday.com to prevent redirect loop, 
              * change back to 'parser.returnHost()' after tests*/
-            Socket s=new Socket(request.returnParser().returnHost(), 80);
-            //Socket s = new Socket("4.53.56.89", 80);
+            Socket s = new Socket(request.returnParser().returnHost(), 80);
+            //Socket s = new Socket("usatoday.com", 80);
             BufferedWriter wt = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
             wt.write(request.returnParser().toHTTPString());
             wt.flush();
@@ -49,28 +53,33 @@ public class MultiThreadedServer implements Runnable {
             /* read response as a stream of bytes with InputStream */
             InputStream binaryReader = s.getInputStream();
             /* we're using a 1MB byte array to store data...will that be enough? what is we need to store more data? */
-            byte[] data=new byte[1048576];
-            int dataRead=binaryReader.read(data);
-            //System.out.print(new String(data));
+            byte[] data = new byte[1048576];
+            int dataRead = binaryReader.read(data);
             /* throw an exception if no response is read from server */
-            if(dataRead==0){
+            if (dataRead == 0 || dataRead == -1) {
                 System.err.println("could not read any data from server");
                 throw new Exception();
             }
+
             /* move binary data into smaller byte array */
-            byte[] data_small= Arrays.copyOfRange(data, 0, dataRead);
+            byte[] data_small = Arrays.copyOfRange(data, 0, dataRead);
             /* mark larger byte array for garbage collection */
-            data=null;
-            
+            data = null;
+
+            System.out.println("read " + dataRead + " bytes from server: " + new String(data_small));
+
+            /* close socket to server */
+            s.close();
+
             /* parse HTTP response and store in an object */
             HTTPResponse response = new HTTPResponse(data_small);
             byte[] totalData = response.toHTTPBytes();
 
             /* write response to client */
-            DataOutputStream os = new DataOutputStream(sock.getOutputStream());
             os.write(totalData);
             os.flush();
 
+            sock.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
